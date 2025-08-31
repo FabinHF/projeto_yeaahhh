fda5acfda5067dc225f01a7029dd964661d96b2d
FabinHF <fabiohirof@gmail.com>
2025-08-31 19:03:56 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -20,8 +20,29 @@ function escolher(botao){
 
 //=================================================================================//
 
-let bulbassauro = ["../img/pokemon/bulbassauro/1","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon"];
-let charmander = ["../img/pokemon/charmander/1","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander"];
+let bulbassauro = ["../img/pokemon/bulbassauro/","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon/bulbasasuro","../img/pokemon"];
+let charmander = ["../img/pokemon/charmander/","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander","../img/pokemon/charmander"];
 let squirtle = ["../img/pokemon/squirtle/1","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle","../img/pokemon/squirtle"];
 
 let caminho = [];
+/* Linha evolutiva dos iniciais ---------------------------------------- /
+const evolucao_bulbassauro = ["Bulbasaur", "Ivysaur", "Venusaur"];
+const pkm_img_bulba_evolucao = ["001", "002", "003"];
+
+const evolucao_charmander = ["Charmander", "Charmeleon", "Charizard"];
+const pkm_img_charmander_evolucao = ["004", "005", "006"];
+
+const evolucao_squirtle = ["Squirtle", "Wartortle", "Blastoise"];
+const pkm_img_squirtle_evolucao = ["007", "008", "009"];
+/ Linha evolutiva dos iniciais ---------------------------------------- /
+
+/ Caminho de cada inicial ---------------------------------------- /
+const caminho_bulbassauro = ["Caterpie","Pidgey","Vulpix","Paras","Mankey","Growlithe","Magmar","Moltres","Mewtwo"];
+const pkm_img_bulbassauro = ["010", "016", "037", "046", "056", "058", "126", "146", "150"];
+/ ------------------------- /
+const caminho_charmander = ["Spearow","Sandshrew","Nidorina","Psyduck","Tentacruel","Golem","Lapras","Articuno","Mewtwo"];
+const pkm_img_charmander = ["021", "027", "030", "054", "073", "076", "131", "144", "150"];
+/ ------------------------- /
+const caminho_squirtle = ["Rattata","Ekans","Pikachu","Victreebel","Voltorb","Scyther","Electabuzz","Zapdos","Mewtwo"];
+const pkm_img_squirtle = ["019", "023", "025", "071", "100", "123", "125", "145", "150"];
+/ Caminho de cada inicial ---------------------------------------- */
