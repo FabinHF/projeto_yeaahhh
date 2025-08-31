4048c3bf8f007699f3028726c87c0441f79e62cd
FabinHF <fabiohirof@gmail.com>
2025-08-31 15:22:47 -0300

    adicionando arquivos nao fotos

---
+++ js/index.js
@@ -0,0 +1,22 @@
+function escolher(botao){
+    if(botao.id == "bulb"){
+        caminho = bulbassauro;
+    }else if(botao.id == "char"){
+        caminho = charmander
+    }else{
+        caminho = squirtle
+    };
+
+};
+
+function tutorial(){
+    document.getElementById("texto").innerHTML = "ttexto";
+};
+
+//=================================================================================//
+
+let bulbassauro = ["b","u","l","b","a","s","s","a","u","r"];
+let charmander = ["c","h","a","r","m","a","n","d","e","r"];
+let squirtle = ["s","q","u","i","r","t","l","e","s","s"];
+
+let caminho = [];
