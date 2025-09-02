5d06112ff07f980d58712eff603e926b58025a46
FabinHF <fabiohirof@gmail.com>
2025-09-02 00:20:57 -0300

    fe

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
- function tutorial(){
+function tutorial(){
     document.getElementById("texto").innerHTML = (`
         PokeTower é uma fangame da franquia Pokémon com toques e elementos da franquia de Mortal Kombat.<br><br>
         O objetivo do jogo é você conseguir levar o seu pokémon inicial até o topo da torre superando seus desafios.<br><br>
@@ -27,6 +27,7 @@ function escolher(botao){
         caminho_nome = caminho_bulbassauro;
         escolha = in_bulb;
         escolha_nome = evolucao_bulbassauro;
+        tipos_adv = tip_bulb;
 
         nome_arq = "bulbassaur"
     }else if(botao.id == "char"){
@@ -34,6 +35,7 @@ function escolher(botao){
         caminho_nome = caminho_charmander;
         escolha = in_char;
         escolha_nome = evolucao_charmander;
+        tipos_adv = tip_char;
 
         nome_arq = "charmander"
     }else{
@@ -41,6 +43,7 @@ function escolher(botao){
         caminho_nome = caminho_squirtle;
         escolha = in_squir;
         escolha_nome = evolucao_squirtle
+        tipos_adv = tip_squir;
 
         nome_arq = "squritle"
     };
@@ -54,7 +57,7 @@ function escolher(botao){
 function montarCampo(){
     if(nivel >= 1 && nivel <=3 ){
         document.getElementById("pkm_ali").innerHTML = escolha[0];
-    }else if(nivel >3 && nivel <= 6){
+    }else if(nivel >3 && nivel <= 7){
         document.getElementById("pkm_ali").innerHTML = escolha[1];
     }else{
         document.getElementById("pkm_ali").innerHTML = escolha[2];
@@ -71,8 +74,8 @@ function montarCampo(){
 
 function batalha(){
     let text = '<img class="ibage" src="../img/pokemon/' + nome_arq +'/' + (nivelTorre +1) +'.png">';
-    document.getElementById("pkm_adv").innerHTML = text;
 
+    document.getElementById("pkm_adv").innerHTML = text;
     document.getElementById("nome_adv").innerHTML = caminho_nome[nivelTorre];
 };
 
@@ -84,22 +87,25 @@ let caminho = [];
 let caminho_nome = [];
 let escolha = [];
 let escolha_nome = [];
+let tipos_adv = [];
+let nome_arq;
 
 // status predefinidos //
 
-let nome_arq;
-let stt_bulb  = [];
-let stt_char  = [];
-let stt_squir  = [];
+let status = [];
+let stt_bulb  = [[1,10],[3,45],[7,121]];
+let stt_char  = [[3,7],[7,30],[12,102]];
+let stt_squir  = [[2,9],[5,40],[10,113]];
+
+let stts_caminho_bulb = [[1,6],[1,7],[2,4],[4,15],[5,23],[5,30],[6,35],[10,74],[12,90]];
+let stts_caminho_char = [[1,7],[2,4],[1,10],[3,20],[4,45],[4,50],[5,42],[9,82],[10,115]];
+let stts_caminho_squir = [[2,5],[1,7],[2,7],[3,17],[4,35],[5,35],[5,40],[10,80],[11,100]];
 
 // Status caminho //
 
-let status = [];
-let nome_ali = "";
-let nivel = 1;
 let hp = 0;
-let tipo = "";
-let fraqueza = "";
+let nivel = 1;
+let nome_ali = "";
 let nivelTorre = 0;
 
 // Caminho imagem //
@@ -131,3 +137,9 @@ const evolucao_squirtle = ["Squirtle", "Wartortle", "Blastoise"];
 const caminho_bulbassauro = ["Caterpie","Pidgey","Vulpix","Paras","Mankey","Growlithe","Magmar","Moltres","Mewtwo"];
 const caminho_charmander = ["Spearow","Sandshrew","Nidorina","Psyduck","Tentacruel","Golem","Lapras","Articuno","Mewtwo"];
 const caminho_squirtle = ["Rattata","Ekans","Pikachu","Victreebel","Voltorb","Scyther","Electabuzz","Zapdos","Mewtwo"];
+
+// tipagem caminhos //
+
+const tip_bulb = [["inseto"],["normal","voador"],["fogo"],["inseto","venenoso"],["lutador"],["fogo"],["fogo"],["fogo","voador"],["psiquico"]];
+const tip_char = [["normal","voador"],["terra"],["venenoso"],["agua"],["agua","venenoso"],["terra","pedra"],["agua","gelo"],["gelo","voador"],["psiquico"]];
+const tip_squir = [["normal"],["venenoso"],["eletrico"],["planta","venenoso"],["eletrico"],["inseto","voador"],["eletrico"],["eletrico","voador"],["psiquico"]];
